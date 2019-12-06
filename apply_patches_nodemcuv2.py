a28c20093978e2ee0c660d4e51be8cc0fa9b7fd3
Fabricio Tamusiunas <fabricio.tamusiunas@gmail.com>
2019-12-06 01:57:51 -0300

    Resolved patches issues related to PlatformIO recent versions

--- apply_patches_nodemcuv2.py
+++ apply_patches_nodemcuv2.py
@@ -1,8 +1,14 @@
-from os.path import join, isfile 
+import json
+import os
+from hashlib import sha1
+from os import walk
+from os.path import (basename, dirname, expanduser, isdir, isfile, join,
+                     realpath, splitdrive, exists) 
+from click.testing import CliRunner
 from platformio import __version__, exception
 from platformio.compat import WINDOWS, hashlib_encode_data
 from platformio.project.config import ProjectConfig
-import os
+
 
 Import("env")
 
@@ -10,8 +16,11 @@ PROJECTPLATFORM = "nodemcuv2"
 
 def get_project_optional_dir(name, default=None):
     project_dir = get_project_dir()
+    print ("project_dir: " + project_dir)
     config = ProjectConfig.get_instance(join(project_dir, "platformio.ini"))
+    # print ("config: " + config)
     optional_dir = config.get("platformio", name)
+    print ("optional_dir: " + optional_dir)
 
     if not optional_dir:
         return default
@@ -25,15 +34,15 @@ def get_project_optional_dir(name, default=None):
     if optional_dir.startswith("~"):
         optional_dir = expanduser(optional_dir)
 
-    return realpath(optional_dir)
-
-
+    print ((optional_dir))
+    return (optional_dir)
 
 def get_project_workspace_dir():
     return get_project_optional_dir("workspace_dir",
                                     join(get_project_dir(), ".pio"))
 
 def get_project_libdeps_dir():
+    print (get_project_optional_dir("libdeps_dir", join(get_project_workspace_dir(), "libdeps")))
     return get_project_optional_dir(
         "libdeps_dir", join(get_project_workspace_dir(), "libdeps"))
 
@@ -49,46 +58,55 @@ PROJECTLIBDEPS = join(get_project_libdeps_dir(), PROJECTPLATFORM)
 PROJECTLIBDEPSPUBSUBCLIENT = join(PROJECTLIBDEPS, "PubSubClient_ID89")
 PROJECTLIBDEPSFUAXMOESP = join(PROJECTLIBDEPS, "FauxmoESP_ID1303")
 
-PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE = join(PROJECTLIBDEPSPUBSUBCLIENT, ".patching-done")
-PROJECTLIBDEPSFUAXMOESPPATCHINGDONE = join(PROJECTLIBDEPSFUAXMOESP, ".patching-done")
+PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE = join(PROJECTLIBDEPSPUBSUBCLIENT, "patching-done")
+PROJECTLIBDEPSFUAXMOESPPATCHINGDONE = join(PROJECTLIBDEPSFUAXMOESP, "patching-done")
 
-if (isfile(PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE) and (isfile(PROJECTLIBDEPSFUAXMOESPPATCHINGDONE))):
+print (PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE)
+print (PROJECTLIBDEPSFUAXMOESPPATCHINGDONE)
+
+if ((env.Execute("ls %s" % PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE) == 0) and (env.Execute("ls %s" % PROJECTLIBDEPSFUAXMOESPPATCHINGDONE) == 0)):
+    print ("Patches previously applied")
     env.Exit(0)
+else:
+    print ("Patches not applied")
+
 
 original_file = join(PROJECTLIBDEPSPUBSUBCLIENT,"src","PubSubClient.h")
 patched_file = join(PROJECTDIR,"patch","PubSubClient.h.patch")
 print (original_file)
 print (patched_file)
-assert isfile(original_file) and isfile(patched_file)
+#assert isfile(original_file) and isfile(patched_file)
 env.Execute("patch %s %s" % (original_file, patched_file))
 
 original_file = join(PROJECTLIBDEPSPUBSUBCLIENT,"src","PubSubClient.cpp")
 patched_file = join(PROJECTDIR,"patch","PubSubClient.cpp.patch")
 print (original_file)
 print (patched_file)
-assert isfile(original_file) and isfile(patched_file)
+#assert isfile(original_file) and isfile(patched_file)
 env.Execute("patch %s %s" % (original_file, patched_file))
 
 original_file = join(PROJECTLIBDEPSFUAXMOESP,"src","fauxmoESP.h")
 patched_file = join(PROJECTDIR,"patch","fauxmoESP.h.patch")
 print (original_file)
 print (patched_file)
-assert isfile(original_file) and isfile(patched_file)
+#assert isfile(original_file) and isfile(patched_file)
 env.Execute("patch %s %s" % (original_file, patched_file))
 
 original_file = join(PROJECTLIBDEPSFUAXMOESP,"src","fauxmoESP.cpp")
 patched_file = join(PROJECTDIR,"patch","fauxmoESP.cpp.patch")
 print (original_file)
 print (patched_file)
-assert isfile(original_file) and isfile(patched_file)
+#assert isfile(original_file) and isfile(patched_file)
 env.Execute("patch %s %s" % (original_file, patched_file))
 
 def _touch(path):
     with open(path, "w") as fp:
         fp.write("")
 
-env.Execute(lambda *args, **kwargs: _touch(PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE))
-env.Execute(lambda *args, **kwargs: _touch(PROJECTLIBDEPSFUAXMOESPPATCHINGDONE))
+env.Execute("touch %s" % (PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE))
+env.Execute("touch %s" % (PROJECTLIBDEPSFUAXMOESPPATCHINGDONE))
+#env.Execute(lambda *args, **kwargs: _touch(PROJECTLIBDEPSPUBSUBCLIENTPATCHINGDONE))
+#env.Execute(lambda *args, **kwargs: _touch(PROJECTLIBDEPSFUAXMOESPPATCHINGDONE))
 
 print ("Patches: --- --- --- --- --- ---")
 
