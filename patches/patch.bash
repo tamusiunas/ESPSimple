2a7cb44b9967f2ba673c1d66f59641b6dc7ca505
Fabricio Tamusiunas <51459553+fabriciotamusiunas@users.noreply.github.com>
2021-04-12 22:11:56 +0100

    patch.bash updated

--- patches/patch.bash
+++ patches/patch.bash
@@ -1,12 +1,11 @@
 VERSION=0.1
-if [ $# -eq 0 ]
+if [ $# -eq 0 -o $# -gt 1 ]
   then
     echo ""
     echo "patch.bash version $VERSION"
     echo "It's an universal patch script that runs using Unix-like OS and Windows (with git-scm or WSL)"
     echo "It uses GNU patch distributed with Unix-like OS or bash provided by git-scm and WSL"
     echo "For Window it requires git-scm <https://git-scm.com> or Windows WSL installed"
-    echo "No arguments supplied"
     echo "Usage: "
     echo "           \$ $0 \"<patch parameters>\""
     echo "Important: \"<patch parameters>\" $(tput bold)MUST$(tput sgr0) have double quotes."
@@ -20,24 +19,18 @@ if [ $# -eq 0 ]
 fi
 
 parameters=$1 # parametes
-echo "parameters : $parameters"
 uname="$(uname -a)"
 os="unix-like"
-echo "parameters: $parameters"
 
 # check if git bash or other MINGW bash is being used
 if [[ $uname == *"MINGW"* ]]; then
   os="mingw"
-  echo "MINGW"
   parameters=$(echo "$parameters" | sed -e "s/\\\/\//g" | sed -r "s/([ |\"|\'])(.):\//\1\/\l\2\//" | sed -r "s/^(.):\//\/\l\1\//")
 # check if WSL 1 or 2 is being used
 elif [[ $uname == *"Microsoft"* ]]; then
   os="wsl"
-  echo "wsl"
   parameters=$(echo "$parameters" | sed -e "s/\\\/\//g" | sed -r "s/([ |\"|\'])(.):\//\1\/mnt\/\l\2\//" | sed -r "s/^(.):\//\/mnt\/\l\1\//")
 fi
-echo "os : $os"
-echo "parameters (converted): $parameters"
 
 # Apply patch
 echo $(patch $parameters)
