fb62c6ede0abceb3cd1a7ea2b58e02be0dda2bb6
Fabricio Tamusiunas <27977748+tamusiunas@users.noreply.github.com>
2021-05-19 03:29:26 +0100

    README.md

--- lib/AmazonAlexa/AmazonAlexaCommons.h
+++ lib/AmazonAlexa/AmazonAlexaCommons.h
@@ -1,3 +1,14 @@
+/**
+ * @file AmazonAlexa.h
+ * @author Fabricio Tamusiunas (fabricio@tamusiunas.com)
+ * @brief Uses FauxmoESP to integrate project with Amazon Alexa
+ * @version 0.1
+ * @date 2019-02-11
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+
 #include "ESPConfig.h"
 #include "GpioManager.h"
 
